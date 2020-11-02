dca9b38eb65ad41834d050d5e71311ce3dc01835
Haobo-X <56521414+Haobo-X@users.noreply.github.com>
2020-11-01 16:13:15 -0800

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -30,17 +30,15 @@ int get_block_size(){
 int mydgetrf(double *A, int *ipiv, int n) 
 {
     /* add your code here */
-    int i, j, k, max_index, in, maxn, tmp2, jn;
-    int size_n = sizeof(double) * n;
+    int i, j, k, max_index, tmp2;
     double max, tmp1;
     // used for swapping rows
-    register double * tmp_row = (double *)malloc(size_n);
+    double * tmp_row = (double *)malloc(sizeof(double) * n);
     
     for (i = 0; i < n; i++)
     {
         max_index = i;
-	in = i * n; 
-        max = fabs(A[in + i]);
+        max = fabs(A[i * n + i]);
         for (j = i + 1; j < n; j++)
         {
             tmp1 = fabs(A[j * n + i]);
@@ -63,20 +61,19 @@ int mydgetrf(double *A, int *ipiv, int n)
             tmp2 = ipiv[i];
             ipiv[i] = ipiv[max_index];
             ipiv[max_index] = tmp2;
-	    maxn = max_index * n; 	
+		
             // swap rows of A
-            memcpy(tmp_row, A + in, size_n);
-            memcpy(A + in, A + maxn, size_n);
-            memcpy(A + maxn, tmp_row, size_n);
+            memcpy(tmp_row, A + i * n, sizeof(double) * n);
+            memcpy(A + i * n, A + max_index * n, sizeof(double) * n);
+            memcpy(A + max_index * n, tmp_row, sizeof(double) * n);
         }
     
         for (j = i + 1; j < n; j++)
         {
-	    jn = j * n;	
-            A[jn + i] = A[jn + i] / A[in + i];
+            A[j * n + i] = A[j * n + i] / A[i * n + i];
             for (k = i + 1; k < n; k++)
             {
-                A[jn + k] -= A[jn + i] * A[in + k];
+                A[j * n + k] -= A[j * n + i] * A[i * n + k];
             }
         }
     }
@@ -115,8 +112,8 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
 {
     /* add your code here */
     int i, j;
-    register double sum = 0;
-    register double * tmp_B = (double *)malloc(sizeof(double) * n);
+    double sum = 0;
+    double * tmp_B = (double *)malloc(sizeof(double) * n);
     
     if (UPLO == 'L')
     {
@@ -157,69 +154,13 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
  * 
  **/
 void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
-{
-/*
-    int i1, j1, k1, i2, j2, k2;
-    for (i1 = 0; i1 < n; i1 += b)
-    {
-        for (j1 = 0; j1 < j; j1 += b)
-        {
-            for (k1 = 0; k1 < i; k1 += b)
-            {
-                for (i2 = i1; i2 < (i1 + b > n? n : (i1 + b)); i2 += 3)
-                {
-                    for (j2 = j1; j2 < (j1 + b > j? j : (j1 + b)); j2 += 3)
-                    {
-                        register double C_0_0 = C[i2 * k + j2];
-                        register double C_0_1 = C[i2 * k + (j2 + 1)];
-                        register double C_0_2 = C[i2 * k + (j2 + 2)];
-                        register double C_1_0 = C[(i2 + 1) * k + j2];
-                        register double C_1_1 = C[(i2 + 1) * k + (j2 + 1)];
-                        register double C_1_2 = C[(i2 + 1) * k + (j2 + 2)];
-                        register double C_2_0 = C[(i2 + 2) * k + j2];                
-                        register double C_2_1 = C[(i2 + 2) * k + (j2 + 1)];
-                        register double C_2_2 = C[(i2 + 2) * k + (j2 + 2)];
-
-                        for (k2 = k1; k2 < (k1 + b > i? i : (k1 + b)); k2++)
-                        {
-                            register double A_0 = A[i2 * k + k2];
-                            register double A_1 = A[(i2 + 1) * k + k2];
-                            register double A_2 = A[(i2 + 2) * k + k2];
-                            register double B_0 = B[k2 * k + j2];
-                            register double B_1 = B[k2 * k + (j2 + 1)];
-                            register double B_2 = B[k2 * k + (j2 + 2)];
-                            
-                            C_0_0 += A_0 * B_0;
-                            C_0_1 += A_0 * B_1;
-                            C_0_2 += A_0 * B_2;
-                            C_1_0 += A_1 * B_0;
-                            C_1_1 += A_1 * B_1;
-                            C_1_2 += A_1 * B_2;
-                            C_2_0 += A_2 * B_0;
-                            C_2_1 += A_2 * B_1;
-                            C_2_2 += A_2 * B_2;
-                        }
-                        
-                        C[i2 * k + j2] = C_0_0;
-                        C[i2 * k + (j2 + 1)] = C_0_1;
-                        C[i2 * k + (j2 + 2)] = C_0_2;
-                        C[(i2 + 1) * k + j2] = C_1_0;
-                        C[(i2 + 1) * k + (j2 + 1)] = C_1_1;
-                        C[(i2 + 1) * k + (j2 + 2)] = C_1_2;
-                        C[(i2 + 2) * k + j2] = C_2_0;
-                        C[(i2 + 2) * k + (j2 + 1)] = C_2_1;
-                        C[(i2 + 2) * k + (j2 + 2)] = C_2_2;
-                    }
-                }
-            }
-        }
-    }
-*/
-    
-    int i1, j1, k1, in1, in2, in3, j2, j3, kn;
-    int n1 = i + b > n ? n : i + b;
-    int n2 = j + b > n ? n : j + b;
-    int n3 = k + b > n ? n : k + b;
+{    
+    register int i1, j1, k1; 
+    // used for optimizing	  
+    register int in1, in2, in3, j2, j3, kn;	
+    register int n1 = i + b > n ? n : i + b;
+    register int n2 = j + b > n ? n : j + b;
+    register int n3 = k + b > n ? n : k + b;
 
     for (i1 = i; i1 < n1; i1 += 3)
     {
@@ -239,34 +180,6 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
             register double C_2_0 = C[in3 + j1];
             register double C_2_1 = C[in3 + j2];
             register double C_2_2 = C[in3 + j3];
-
-
-            for (k1 = k; k1 < n3; k1 += 3)
-            {
-		int l;
-                for (l = 0; l < 3; l++)
-                {
-                    int k2 = k1 + l;
-                    int k3 = k1 * n + j1 + l * n;
-                    register double A_0 = A[in1 + k2];
-                    register double A_1 = A[in2 + k2];
-                    register double A_2 = A[in3 + k2];
-                    register double B_0 = B[k3];
-                    register double B_1 = B[k3 + 1];
-                    register double B_2 = B[k3 + 2];
-
-                    C_0_0 -= A_0 * B_0;
-                    C_0_1 -= A_0 * B_1;
-                    C_0_2 -= A_0 * B_2;
-                    C_1_0 -= A_1 * B_0;
-                    C_1_1 -= A_1 * B_1;
-                    C_1_2 -= A_1 * B_2;
-                    C_2_0 -= A_2 * B_0;
-                    C_2_1 -= A_2 * B_1;
-                    C_2_2 -= A_2 * B_2;
-                }
-            }
-	    
 		
 	    for (k1 = k; k1 < n3; k1++)
             {
@@ -288,33 +201,7 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
                 C_2_1 -= A_2 * B_1;
                 C_2_2 -= A_2 * B_2;
             }
-	
-/*		
-            for (k1 = 0; k1 < b; k1++)
-            {
-                register int j00 = i1 * k + k1;
-                register int k00 = k1 * k + j1;
-
-                register double A_0_M = A[j00];
-                register double A_1_M = A[j00 + k];
-                register double A_2_M = A[j00 + 2 * k];
-
-                register double B_M =  B[k00];
-                C_0_0 -= A_0_M * B_M;
-                C_1_0 -= A_1_M * B_M;
-                C_2_0 -= A_2_M * B_M;
-
-                B_M = B[k00 + 1];
-                C_0_1 -= A_0_M * B_M;
-                C_1_1 -= A_1_M * B_M;
-                C_2_1 -= A_2_M * B_M;
-
-                B_M = B[k00 + 2];
-                C_0_2 -= A_0_M * B_M;
-                C_1_2 -= A_1_M * B_M;
-                C_2_2 -= A_2_M * B_M;
-            }	
-*/		
+		
             C[in1 + j1] = C_0_0;
             C[in1 + j2] = C_0_1;
             C[in1 + j3] = C_0_2;
@@ -359,18 +246,21 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
  **/
 int mydgetrf_block(double *A, int *ipiv, int n, int b) 
 {
-    int ib, ib2, i, n1, j, k, max_index, in, maxn, tmp2, jn;
-    int size_n = sizeof(double) * n;
-    double max, tmp1, sum;
-    double *temprow = (double *) malloc(size_n);
+    register int ib, i, j, k, max_index, tmp2;
+    register double max, tmp1, sum;	
+    // used for optimizing
+    register int ib2, n1, in, maxn, jn; 	
+    register int size_n = sizeof(double) * n;
+    // used for swapping rows	
+    register double *temprow = (double *) malloc(size_n);
 
+    // A(ib:n, ib:end) = P' * L' * U'		
     for (ib = 0; ib < n; ib += b)
     {
 	ib2 = ib + b;    
 	n1 = ib2 > n ? n : ib2;   
         for (i = ib; i < n1; i++)
         {
-            // pivoting
             max_index = i;
 	    in = i * n;
             max = fabs(A[in + i]);
@@ -395,19 +285,17 @@ int mydgetrf_block(double *A, int *ipiv, int n, int b)
             {    
                 if (max_index != i)
                 {
-                    // save pivoting information
                     tmp2 = ipiv[i];
                     ipiv[i] = ipiv[max_index];
-                    ipiv[max_index] = tmp2;
-		    maxn = max_index * n;    	
+                    ipiv[max_index] = tmp2;    	
                     // swap rows
+		    maxn = max_index * n;	
                     memcpy(temprow, A + in, size_n);
                     memcpy(A + in, A + maxn, size_n);
                     memcpy(A + maxn, temprow, size_n);
                 }
             }
 
-            // factorization
             for (j = i + 1; j < n; j++)
             {
 		jn = j * n;    
@@ -420,7 +308,7 @@ int mydgetrf_block(double *A, int *ipiv, int n, int b)
             }
         }
 
-        // update A(ib:end, end+1:n)
+        // A(ib:end, end+1:n) = LL-1 * A(ib:end, end+1:n)
         for (i = ib; i < n1; i++)
         {
 	    in = i * n;	
@@ -435,7 +323,7 @@ int mydgetrf_block(double *A, int *ipiv, int n, int b)
             }
         }
 
-        // update A(end+1:n, end+1:n)
+        // A(end+1:n, end+1:n) -= A(end+1:n, ib:end) * A(ib:end, end+1:n)
         for (i = ib2; i < n; i += b)
         {
             for (j = ib2; j < n; j += b)
